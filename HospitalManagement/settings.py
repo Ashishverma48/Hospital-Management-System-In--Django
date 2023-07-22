563694183f6c1fee4fb85147349acf4537232eea
ASHISH VERMA <ashishkv845@gmail.com>
2023-07-22 11:54:14 +0530

    update

--- HospitalManagement/settings.py
+++ HospitalManagement/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mw9n4ohf9-=ve^bvb!^njd*b45v-4ikep3%i^lwjaps^k&o83z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['ashishverma.pythonanywhere.com]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
